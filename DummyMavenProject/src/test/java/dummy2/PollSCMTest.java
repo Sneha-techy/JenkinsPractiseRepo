e49e45abfdb2dbb09dd0972b9f428b2cd4c4dff8
sneha <sneha@LAPTOP-SB7RE8KB>
2025-04-21 10:53:40 +0530

    PollSCMTest class is Executed

---
+++ DummyMavenProject/src/test/java/dummy2/PollSCMTest.java
@@ -0,0 +1,12 @@
+package dummy2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+	@Test
+	public void test() {
+		Reporter.log("PollSCMTest class is Executed", true);
+	}
+	
+}
